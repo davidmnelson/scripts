b8512cf967a1c2c6eab262ea0701ddf44b604636
David Nelson <github@davidnelson.net>
2017-10-04 21:17:24 -0700

    Update enable-texttospeech.sh

--- misc/enable-texttospeech.sh
+++ misc/enable-texttospeech.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-# Enables text-to-speech and sets Control-R hotkey.
+# Enable text-to-speech and set Control-R shortcut. 
+# Intended to run as a login-every script via Outset.
 
 /usr/bin/defaults write com.apple.speech.synthesis.general.prefs SpokenUIUseSpeakingHotKeyCombo -int 4111
 
 /usr/bin/defaults write com.apple.speech.synthesis.general.prefs SpokenUIUseSpeakingHotKeyFlag -bool true
-
-exit 0
