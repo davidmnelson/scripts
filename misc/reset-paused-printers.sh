cce3dfb53f5612fdb51322b97b6123e21e91b201
David Nelson <github@davidnelson.net>
2019-02-05 15:22:45 -0800

    Update reset-paused-printers.sh

--- misc/reset-paused-printers.sh
+++ misc/reset-paused-printers.sh
@@ -2,7 +2,7 @@
 
 /usr/bin/lpstat -p | /usr/bin/grep -EB1 "Paused" | /usr/bin/grep "^printer" | while read line; do
 
-queue=`/bin/echo $line | /usr/bin/cut -d" " -f2`
+queue=`/bin/echo $line | /usr/bin/awk -F ' ' '{print $2}'`
 
 	for job in `/usr/bin/lpq -a -P $queue | /usr/bin/awk -F ' ' '{if (NR>1) {print $3}}' | /usr/bin/grep -v "^Job"`; do
 		/usr/bin/lprm $job ; 
