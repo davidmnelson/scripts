c1ee155075ef98ebe90ca8ff96ece238415704d9
David Nelson <github@davidnelson.net>
2019-02-05 14:45:59 -0800

    Added reset-paused-printers.sh
    Script looks for paused printers, clears all jobs from the queue, then resumes. Most useful if run automatically (outset, a LaunchDaemon, etc.)

---
+++ reset-paused-printers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+/usr/bin/lpstat -p | /usr/bin/grep -EB1 "Paused" | /usr/bin/grep "^printer" | while read line; do
+
+queue=`/bin/echo $line | /usr/bin/cut -d" " -f2`
+
+	for job in `/usr/bin/lpq -a -P $queue | /usr/bin/awk -F ' ' '{if (NR>1) {print $3}}' | /usr/bin/grep -v "^Job"`; do
+		/usr/bin/lprm $job ; 
+	done
+
+/usr/sbin/cupsenable $queue
+
+done
